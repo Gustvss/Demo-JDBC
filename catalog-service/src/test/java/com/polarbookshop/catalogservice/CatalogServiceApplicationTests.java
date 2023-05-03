ccc9e94be770d9574a2d2edaed99879d3304a8fe
Gustavo Sant Ana <gustvss.dev@gmail.com>
2023-05-03 10:07:35 -0300

    initial commit

---
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.polarbookshop.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//Fornece uma configuração para testar aplicativos Spring Boot
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	//Identifica um caso de teste
+	@Test
+	//Teste vazio usado para verificar se o contexto do aplicativo foi carregado corretamente
+	void contextLoads() {
+	}
+
+}
